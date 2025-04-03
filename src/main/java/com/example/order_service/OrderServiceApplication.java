1f60e11b71a5738b3c77160340083445cb0bdc52
michaeljosephroddy <michaeljosephroddy@gmail.com>
2025-04-03 12:36:25 +0100

    debug jdbc url

--- src/main/java/com/example/order_service/OrderServiceApplication.java
+++ src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.order_service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +10,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackages = { "com.example.order_service", "com.example.hateoas" }) // Ensure correct package
-public class OrderServiceApplication {
+public class OrderServiceApplication implements CommandLineRunner {
+
+	@Value("${spring.datasource.url}")
+	private String jdbcUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("JDBC URL: " + jdbcUrl);
 	}
 
 }
